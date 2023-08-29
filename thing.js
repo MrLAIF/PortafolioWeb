0ae2b9894f147887bc13b02faadb8396c6988e75
MrLAIF <erikcavalos782.dev@gmail.com>
2023-08-28 23:06:28 -0500

    sniff

--- thing.js
+++ thing.js
@@ -2,13 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   const showButton = document.getElementById("showElementButton");
   const hiddenElement = document.getElementById("hiddenElement");
   const buttondiv = document.getElementById("buttondiv");
-  const hiddengif = document.getElementById("hiddengif");
+  const hiddengif = document.querySelector(".centerGIF");
   
   showButton.addEventListener("click", function() {
-    hiddenElement.style.display = "block"; // Change to "block" to make it visible
+    hiddenElement.style.display = "flex"; // Change to "block" to make it visible
     buttondiv.style.display = "none"; // Change to "block" to make it visible
-    hiddengif.style.display = "block"; // Change to "block" to make it visible
   });
 });
-
-  
