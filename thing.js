05c95c786e7bef024455130f2690413b5726e776
MrLAIF <erikcavalos782.dev@gmail.com>
2023-08-30 02:44:27 -0500

    JS blegh

--- thing.js
+++ thing.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
   const presentacionTitulo =document.getElementById("principal")
   const presentacionDes =document.getElementById("principal-des")
   const catGIF=document.getElementById("catJAMGif")
-
+  const barThing=document.getElementById("bar")
+  
 
   showButton.addEventListener("click", function() {
     document.body.style.animation="backflash 0.5s forwards";
@@ -15,7 +16,8 @@ document.addEventListener("DOMContentLoaded", function() {
     
     presentacionTitulo.style.animation = "backflash-text 0.5s forwards, slideInLeft 0.5s ease-in-out 0.3s 1 normal both";
     presentacionDes.style.animation = "backflash-text 0.5s forwards, slideInLeft 0.5s ease-in-out 0.8s 1 normal both";
-    catGIF.style="background-image: url(./catJAMgif.gif);"
+    catGIF.style.animation="transparent 2.5s forwards";
+    barThing.style.animation="transparent 2.5s forwards";
 
   });
 });
